913f2c3770566d67521cac65b8b8c2a1450d867f
Mitchell Grice <mitch@gricey.net>
2016-12-11 16:32:50 +1000

    Puzzle 6

---
+++ 6_2.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+# Read file
+with open('6.in', 'r') as f:
+    lines = f.readlines()
+lines = map(str.strip, lines)
+
+# Process
+print ''.join([c.most_common()[-1][0] for c in map(Counter, [list(x) for x in zip(*lines)])])
